01347f00e9a636c0e485e1c7405353ecfdf9d3a8
mball <mball@redwoodsys.com>
2011-10-01 13:15:46 -0700

    logout

--- serve/meduele_server.py
+++ serve/meduele_server.py
@@ -180,6 +180,7 @@ def twilio_incoming_callback():
     # not sure what to return here..
     return flask.redirect(flask.url_for('show_home'))
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     error = None
@@ -232,10 +233,6 @@ def logout():
     flask.flash('you logged out, adios')
     return flask.redirect(flask.url_for('show_home'))
 
-@app.route('/test')
-def logout():
-    error = None
-    return flask.redirect(flask.url_for('show_test'))
 
 @app.route('/')
 def show_home():
