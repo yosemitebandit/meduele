8509a4c7e86153291557627b5c52af929a2d60ea
Stephanie Chang <chang.steph.h@gmail.com>
2011-10-01 12:03:49 -0700

    added twiliocapability to browser_call development

--- browser_calls/hello_client.py
+++ browser_calls/hello_client.py
@@ -1,11 +1,18 @@
+import os
 from twilio.rest import TwilioRestClient
+from twilio.util import TwilioCapability
 
-account = "AXXXXXXXXXXXXXXXXX"
-token = "YYYYYYYYYYYYYYYYYY"
+settings_path = os.environ.get('MEDUELE_SETTINGS')
+execfile(settings_path)
 
-client = TwilioRestClient(account, token)
-call = client.calls.create( to="3093609866",
-                            from_="3093609866", 
-                            url="http://twilio.nfshost.com/med/hello-client-twiml.php")
-print call.length
-print call.sid
+client_name = "jenny"
+
+# client = TwilioRestClient(account, token)
+# call = client.calls.create( to="9196225123",
+#                             from_="3093609866", 
+#                             url="http://twilio.nfshost.com/med/hello-client-twiml.php")
+
+capability = TwilioCapability(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+capability.allow_client_outgoing(TWILIO_APP_SID)
+capability.allow_client_incoming(client_name)
+token = capability.generate()
